4150d534538a93c855eedb9be8eb845b170bac61
Daniel Kogel <dkogel123@gmail.com>
2022-01-05 21:20:40 -0600

    added route that's pulling data as a json

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask
 from flask import render_template
 from flask import jsonify
-from flask import send_from directory
+from flask import send_from_directory
 
 #import functions from SQL Alchemy
 import sqlalchemy
@@ -17,7 +17,7 @@ import json
 username = 'postgres'
 password = 'bootcamp'
 database_name = 'wildfire_db'
-connection_string = f'postgresql://{username}:{password}@localhost:5432{database_name}'
+connection_string = f'postgresql://{username}:{password}@localhost:5432/{database_name}'
 
 #connect to database
 engine = create_engine(connection_string)
@@ -34,4 +34,33 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")
 def IndexRoute():
     homepage = render_template("index.html")
+    
+
     return homepage
+
+@app.route("/data")
+def DataRoute():
+
+    
+
+
+    session = Session(engine)
+    results = session.query(table.fire_size, table.latitude).all()
+    session.close
+
+    fire_data = []
+    
+    for fire_size, latitude in results:
+        dict = {}
+        dict["fire_size"] = fire_size
+        dict["latitude"] = latitude
+        fire_data.append(dict)
+    
+
+    return jsonify(fire_data)
+
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
