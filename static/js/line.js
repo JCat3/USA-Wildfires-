ad98c00be9eecba04349b7e30b4cf39d2604e6e2
JCat3 <jcatalano333@yahoo.com>
2022-01-10 00:14:01 -0600

    changed file name in line.html

--- static/js/line.js
+++ static/js/line.js
@@ -1,106 +1,88 @@
-//animated line chart with fire size, precipitation by date - include 2 axis 
 const ctx = document.getElementById("myChart").getContext("2d");
 
-console.log("line.js is loaded")
+console.log("line2.js is loaded")
 
 function drawLine() {
     d3.json("/lineGraph_data").then(yearData => {
         console.log(yearData);
 
-    //map the years to a list
-    let years = yearData.map(o => o.Date);
-    console.log(years)
+        //map the years to a list
+        let years = yearData.map(o => o.Date);
+        console.log(years)
 
-    //map the fire size to a list
-    let fire_size = yearData.map(o => o.Fire_Size);
-    console.log(fire_size);
+        //map the fire size to a list
+        let fire_size = yearData.map(o => o.Fire_Size);
+        console.log(fire_size);
 
-    let precip = yearData.map(o => o.Precipitation);
-    console.log(precip);
+        let precip = yearData.map(o => o.Precipitation);
+        console.log(precip);
 
-    const labels = years;
+        const labels = years;
 
-    const data = {
-        labels,
-        datasets: [
-            {
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: "Total Acres Burned",
                 data: fire_size,
-                label: "Fire Size",
-            },
-            {
-                data2: precip,
-                label2: "Precipitation",
-            }
-        ],
-    }
+                backgroundColor: ['rgba(255,26,104,0.2)'],
+                borderColor: ['rgba(255,26,104,1)'],
+                borderWidth: 1,
+                yAxisID: 'acres'
+            }, {
+                label: "Average Precipitation (mm)",
+                data: precip,
+                backgroundColor: ['rgba(0,26,104,0.2)'],
+                borderColor: ['rgba(0,26,104,1)'],
+                borderWidth: 1,
+                xAxisID: 'mm'
+            }]
+        };
 
-    // insert animation documentation from chartjs
-    const totalDuration = 10000;
-    const delayBetweenPoints = totalDuration / data.length;
-    const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
-    const animation = {
-        x: {
-            type: 'number',
-            easing: 'linear',
-            duration: delayBetweenPoints,
-            from: NaN, // the point is initially skipped
-            delay(ctx) {
-                if (ctx.type !== 'data' || ctx.xStarted) {
-                    return 0;
+        const config = {
+            type: 'line',
+            data,
+            options: {
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Total Acres Burned and Average Rainfall per Year'
+                    }
+                },
+                responsive: true,
+                scales: {
+                    acres: {
+                        beginAtZero: true,
+                        type: 'linear',
+                        position: 'left',
+                        suggestedMin: 0,
+                        ticks: {
+                            callback: function(value, index, values) {
+                                return `${value} acres`;
+                            }
+                        }
+                    },
+                    mm: {
+                        beginAtZero: true,
+                        type: 'linear',
+                        position: 'right',
+                        suggestedMin: 0,
+                        ticks: {
+                            callback: function(value, index, values) {
+                                return `${value} mm`;
+                            }
+                        }
+                    }
                 }
-                ctx.xStarted = true;
-                return ctx.index * delayBetweenPoints;
             }
-        },
-        y: {
-            type: 'number',
-            easing: 'linear',
-            duration: delayBetweenPoints,
-            from: previousY,
-            delay(ctx) {
-                if (ctx.type !== 'data' || ctx.yStarted) {
-                    return 0;
-                }
-                ctx.yStarted = true;
-                return ctx.index * delayBetweenPoints;
-            }
-        }   
-    };
+        };
 
-// start animate line chart with documentation from chartjs.org
-    const config = {
-        type: 'line',
-        data: {
-          datasets: [{
-            borderColor: 'red',
-            borderWidth: 1,
-            radius: 0,
-            data: fire_size,
-          },
-          {
-            borderColor: 'blue',
-            borderWidth: 1,
-            radius: 0,
-            data: precip,
-          }]
-        },
-        options: {
-          animation,
-          interaction: {
-            intersect: false
-          },
-          plugins: {
-            legend: false
-          },
-          scales: {
-            x: {
-              type: 'linear'
-            }
-          }
-        }
-      };
+        const myChart = new Chart (
+            document.getElementById('myChart'),
+            config
+        );
 
-    });
+    }); 
+
 }
 
 drawLine();
