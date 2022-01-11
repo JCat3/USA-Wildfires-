19ac923a4c29c928afd9350dfcdfd724b7c54a06
JCat3 <jcatalano333@yahoo.com>
2022-01-10 19:28:02 -0600

    Both fire size and precip data are showing on the graph

--- static/js/line.js
+++ static/js/line.js
@@ -27,21 +27,70 @@ function drawLine() {
                 backgroundColor: ['rgba(255,26,104,0.2)'],
                 borderColor: ['rgba(255,26,104,1)'],
                 borderWidth: 1,
-                yAxisID: 'acres'
-            }, {
+                yAxisID: 'acres',
+            },{
                 label: "Average Precipitation (mm)",
                 data: precip,
                 backgroundColor: ['rgba(0,26,104,0.2)'],
                 borderColor: ['rgba(0,26,104,1)'],
                 borderWidth: 1,
-                xAxisID: 'mm'
+                yAxisID: 'mm',
             }]
+        };
+
+        const totalDuration = 10000;
+        const delayBetweenPoints = totalDuration / precip.length;
+        const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
+        const animation = {
+            x: {
+                type: 'number',
+                easing: 'linear',
+                duration: delayBetweenPoints,
+                from: NaN, // the point is initially skipped
+                delay(ctx) {
+                    if (ctx.type !== 'data' || ctx.xStarted) {
+                        return 0;
+                    }
+                    ctx.xStarted = true;
+                    return ctx.index * delayBetweenPoints;
+                }
+            },
+            y: {
+                type: 'number',
+                easing: 'linear',
+                duration: delayBetweenPoints,
+                from: previousY,
+                delay(ctx) {
+                    if (ctx.type !== 'data' || ctx.yStarted) {
+                        return 0;
+                    }
+                    ctx.yStarted = true;
+                    return ctx.index * delayBetweenPoints;
+                }
+            }   
         };
 
         const config = {
             type: 'line',
             data,
+            //     datasets: [{
+            //         label: "Total Acres Burned",
+            //         data: fire_size,
+            //         backgroundColor: ['rgba(255,26,104,0.2)'],
+            //         borderColor: ['rgba(255,26,104,1)'],
+            //         borderWidth: 1,
+            //         yAxisID: 'acres'
+            //     }, {
+            //         label: "Average Precipitation (mm)",
+            //         data: precip,
+            //         backgroundColor: ['rgba(0,26,104,0.2)'],
+            //         borderColor: ['rgba(0,26,104,1)'],
+            //         borderWidth: 1,
+            //         xAxisID: 'mm'
+            //     }]
+            // },
             options: {
+                // animation,
                 plugins: {
                     title: {
                         display: true,
@@ -50,34 +99,34 @@ function drawLine() {
                 },
                 responsive: true,
                 scales: {
-                    acres: {
+                    yAxis: [{
+                        id: 'acres',
                         beginAtZero: true,
                         type: 'linear',
                         position: 'left',
                         suggestedMin: 0,
                         ticks: {
-                            callback: function(value, index, values) {
+                            callback: function(value) {
                                 return `${value} acres`;
                             }
                         }
-                    },
-                    mm: {
+                    },{
+                        id: 'mm',
                         beginAtZero: true,
                         type: 'linear',
                         position: 'right',
                         suggestedMin: 0,
                         ticks: {
-                            callback: function(value, index, values) {
-                                return `${value} mm`;
+                            callback: function(values) {
+                                return `${values} mm`;
                             }
                         }
-                    }
+                    }]
                 }
             }
         };
 
-        const myChart = new Chart (
-            document.getElementById('myChart'),
+        const myChart = new Chart (ctx, 
             config
         );
 
